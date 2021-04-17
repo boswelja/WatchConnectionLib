6fa1e39a91ea85f8bc70996279b6a020a36ff914
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-04-17 18:26:27 +1200

    Update code comments

--- core/src/main/kotlin/com/boswelja/watchconnection/core/Watch.kt
+++ core/src/main/kotlin/com/boswelja/watchconnection/core/Watch.kt
@@ -2,10 +2,19 @@ package com.boswelja.watchconnection.core
 
 import java.util.UUID
 
+/**
+ * The standardised Watch representation.
+ * @param id A unique ID assigned to this watch. See [UUID].
+ * @param name The display name of this watch.
+ * @param platformId The ID of this watch assigned by it's platform. You should not need to use
+ * this outside of the calling platform.
+ * @param platform The platform identifier string of this watch. You shouldn't need to use this
+ * outside the calling platform.
+ */
 data class Watch(
     val id: UUID,
-    val platformId: String,
     val name: String,
+    val platformId: String,
     val platform: String
 ) {
 
--- core/src/main/kotlin/com/boswelja/watchconnection/core/WatchConnectionInterface.kt
+++ core/src/main/kotlin/com/boswelja/watchconnection/core/WatchConnectionInterface.kt
@@ -3,23 +3,23 @@ package com.boswelja.watchconnection.core
 interface WatchConnectionInterface {
     
     /**
-     * An observable list of all available watches.
+     * A list of all available watches for this platform.
      */
     val availableWatches: List<Watch>
 
     /**
-     * Returns a string unique to the platform handling the connections. It's important this is
-     * constant as it will be stored with registered watches.
+     * Returns a unique string to identify this platform. This will be used to map watches to the
+     * correct platform as needed.
      */
     val platformIdentifier: String
 
     /**
      * Send a message to a watch with the given ID.
-     * @param watchId See [Watch.id].
-     * @param path The message path to send.
+     * @param watchId See [Watch.platformId].
+     * @param message The message to send.
      * @param data The data to send with the message, if any.
      */
-    suspend fun sendMessage(watchId: String, path: String, data: ByteArray? = null)
+    suspend fun sendMessage(watchId: String, message: String, data: ByteArray? = null)
 
     /**
      * Manually refresh info such as watch status and available watches.
