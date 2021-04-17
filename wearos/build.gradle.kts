ad687eccaa7e05b233f96e8a9438f32b27b976ce
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-04-17 19:21:09 +1200

    Add publishing support to wearos module

--- wearos/build.gradle.kts
+++ wearos/build.gradle.kts
@@ -1,6 +1,10 @@
+import Publishing.configureMavenPublication
+
 plugins {
     id("com.android.library")
     kotlin("android")
+    id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -27,4 +31,39 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.4.3")
     implementation("com.google.android.gms:play-services-wearable:17.0.0")
+}
+
+// Bundle sources with binaries
+val androidSourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(android.sourceSets["main"].kotlin.name)
+}
+artifacts {
+    archives(androidSourcesJar)
+}
+
+publishing {
+    publications {
+        create(
+            "release",
+            configureMavenPublication(
+                "wearos",
+                "Wear OS support for Watch Connection Library",
+                "https://github.com/boswelja/WatchConnectionLib/blob/main/wearos",
+                project.configurations.implementation.get().allDependencies
+            ) {
+                artifact("$buildDir/outputs/aar/${project.name}-release.aar")
+                artifact(androidSourcesJar)
+            }
+        )
+    }
+    repositories(Publishing.repositories)
+}
+
+// Create signing config
+ext["signing.keyId"] = Publishing.signingKeyId
+ext["signing.password"] = Publishing.signingPassword
+ext["signing.secretKeyRingFile"] = Publishing.signingSecretKeyring
+signing {
+    sign(publishing.publications)
 }
