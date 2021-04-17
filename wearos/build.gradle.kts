00107560650de402fe90e07731c1a4c7b18c7057
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-04-17 19:28:08 +1200

    Use Maven to fetch a specific version of our core components

--- wearos/build.gradle.kts
+++ wearos/build.gradle.kts
@@ -27,7 +27,7 @@ android {
 }
 
 dependencies {
-    implementation(project(":core"))
+    implementation("io.github.boswelja.watchconnection:core:0.1.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.4.3")
     implementation("com.google.android.gms:play-services-wearable:17.0.0")
