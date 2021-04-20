807e4d830d56b9f27ba95dafb88e06d1b4c32e83
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-04-20 16:03:56 +1200

    Create basic WatchConnectionClientTest

---
+++ test/src/test/kotlin/com/boswelja/watchconnection/core/DummyConnectionHandler.kt
@@ -0,0 +1,31 @@
+package com.boswelja.watchconnection.core
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+
+class DummyConnectionHandler(
+    override val platformIdentifier: String,
+    val allWatches: Array<Watch>,
+    val watchesWithApp: Array<Watch>
+) : PlatformConnectionHandler {
+
+    override fun allWatches(): Flow<Watch> = flowOf(*allWatches)
+
+    override fun watchesWithApp(): Flow<Watch> = flowOf(*watchesWithApp)
+
+    override suspend fun getCapabilitiesFor(watchId: String): Flow<String> = flow { }
+
+    override suspend fun sendMessage(watchId: String, message: String, data: ByteArray?): Boolean {
+        // Do nothing
+        return false
+    }
+
+    override fun registerMessageListener(listener: MessageListener) {
+        // Do nothing
+    }
+
+    override fun unregisterMessageListener(listener: MessageListener) {
+        // Do nothing
+    }
+}
---
+++ test/src/test/kotlin/com/boswelja/watchconnection/core/WatchConnectionClientTest.kt
@@ -0,0 +1,127 @@
+package com.boswelja.watchconnection.core
+
+import android.os.Build
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.mockk.coVerify
+import io.mockk.spyk
+import io.mockk.verify
+import java.util.UUID
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.R])
+class WatchConnectionClientTest {
+    private val dummyWatchCountPerPlatform = 5
+    private val dummyWatchWithAppCountPerPlatform = 3
+
+    private val dummyPlatformIds = arrayOf("platform1", "platform2")
+
+    private var dummyWatches: Array<Watch> = emptyArray()
+    private var dummyWatchesWithApp: Array<Watch> = emptyArray()
+    private var dummyPlatforms: Array<PlatformConnectionHandler> = emptyArray()
+
+    private lateinit var connectionClient: WatchConnectionClient
+
+    @Before
+    fun setUp() {
+        // Clear all existing stuff
+        dummyWatches = emptyArray()
+        dummyWatchesWithApp = emptyArray()
+        dummyPlatforms = emptyArray()
+
+        dummyPlatformIds.forEach { platform ->
+            // Generate dummy watches
+            var allWatches = emptyArray<Watch>()
+            (1..dummyWatchCountPerPlatform + 1).forEach { count ->
+                allWatches += Watch(
+                    name = "Watch $count",
+                    platformId = "watch-$count",
+                    platform = platform
+                )
+            }
+
+            // Generate dummy watches with app
+            var watchesWithApp = emptyArray<Watch>()
+            (1..dummyWatchWithAppCountPerPlatform + 1).forEach { count ->
+                watchesWithApp += Watch(
+                    name = "Watch $count",
+                    platformId = "watch-$count",
+                    platform = platform
+                )
+            }
+
+            // Add to totals
+            dummyWatches += allWatches
+            dummyWatchesWithApp += watchesWithApp
+
+            // Create dummy connection handlers
+            dummyPlatforms += spyk(DummyConnectionHandler(platform, allWatches, watchesWithApp))
+        }
+
+        connectionClient = WatchConnectionClient(*dummyPlatforms)
+    }
+
+    @Test
+    fun `allWatches gets all watches from platforms`() {
+        connectionClient.allWatches()
+        dummyPlatforms.forEach { verify { it.allWatches() } }
+    }
+
+    @Test
+    fun `watchesWithApp gets all watches with app from platforms`() {
+        connectionClient.watchesWithApp()
+        dummyPlatforms.forEach { verify { it.watchesWithApp() } }
+    }
+
+    @Test
+    fun `sendMessage passes request to the correct platform`(): Unit = runBlocking {
+        val message = "message"
+        dummyWatches.forEach { watch ->
+            connectionClient.sendMessage(
+                watch,
+                message = message
+            )
+            val platform = dummyPlatforms.first { it.platformIdentifier == watch.platform }
+            coVerify {
+                platform.sendMessage(watch.platformId, message, null)
+            }
+        }
+    }
+
+    @Test
+    fun `getCapabilitiesFor passes request to the correct platform`(): Unit = runBlocking {
+        dummyWatches.forEach { watch ->
+            connectionClient.getCapabilitiesFor(watch)
+            val platform = dummyPlatforms.first { it.platformIdentifier == watch.platform }
+            coVerify { platform.getCapabilitiesFor(watch.platformId) }
+        }
+    }
+
+    @Test
+    fun `registerMessageListener adds the message listener to all platforms`() {
+        val messageListener = object : MessageListener {
+            override fun onMessageReceived(sourceWatchId: UUID, message: String, data: ByteArray?) { }
+        }
+        connectionClient.registerMessageListener(messageListener)
+        dummyPlatforms.forEach { platform ->
+            verify { platform.registerMessageListener(messageListener) }
+        }
+    }
+
+    @Test
+    fun `unregisterMessageListener removes the message listener from all platforms`() {
+        val messageListener = object : MessageListener {
+            override fun onMessageReceived(sourceWatchId: UUID, message: String, data: ByteArray?) { }
+        }
+        connectionClient.unregisterMessageListener(messageListener)
+        dummyPlatforms.forEach { platform ->
+            verify { platform.unregisterMessageListener(messageListener) }
+        }
+    }
+}
