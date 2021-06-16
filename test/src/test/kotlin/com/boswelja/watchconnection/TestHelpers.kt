e3b5c36c09922e3239c61ac291835f6cc9d9a3f3
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-16 14:26:34 +1200

    Create WearOSMessagePlatformTest.kt

--- test/src/test/kotlin/com/boswelja/watchconnection/TestHelpers.kt
+++ test/src/test/kotlin/com/boswelja/watchconnection/TestHelpers.kt
@@ -1,6 +1,8 @@
 package com.boswelja.watchconnection
 
 import com.boswelja.watchconnection.core.Watch
+import com.boswelja.watchconnection.core.message.Message
+import kotlin.random.Random
 
 fun createWatchesFor(count: Int, platformIdentifier: String): List<Watch> {
     return (0..count).map {
@@ -14,4 +16,21 @@ fun createWatchesFor(count: Int, platformIdentifier: String): List<Watch> {
 
 fun createCapabilities(count: Int): List<String> {
     return (0..count).map { "capability$it" }
+}
+
+/**
+ * Returns a list of pairs. Each pair contains the source watch ID (note this is not the watches UID
+ * assigned by WatchConnectionLib), and a fake [Message].
+ */
+fun createMessagesFor(count: Int, platform: String): List<Pair<String, Message>> {
+    return (0..count).map {
+        Pair(
+            it.toString(),
+            Message(
+                Watch.createUUID(platform, it.toString()),
+                it.toString(),
+                Random.nextBytes(8)
+            )
+        )
+    }
 }
---
+++ test/src/test/kotlin/com/boswelja/watchconnection/wearos/rules/MessageClientTestRule.kt
@@ -0,0 +1,46 @@
+package com.boswelja.watchconnection.wearos.rules
+
+import com.google.android.gms.tasks.Tasks
+import com.google.android.gms.wearable.MessageClient
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+class MessageClientTestRule : TestRule {
+
+    private var messageListener: MessageClient.OnMessageReceivedListener? = null
+
+    lateinit var messageClient: MessageClient
+
+    override fun apply(base: Statement?, description: Description?): Statement {
+        return MessageClientTestRuleStatement(base)
+    }
+
+    fun receiveMessage(sourceNodeId: String, message: String, data: ByteArray? = null) {
+        if (messageListener == null)
+            throw NullPointerException("No message listener has been added")
+
+        messageListener!!.onMessageReceived(DummyMessageEvent(sourceNodeId, message, data))
+    }
+
+    inner class MessageClientTestRuleStatement(private val base: Statement?) : Statement() {
+        override fun evaluate() {
+            // Set up MessageClient mock
+            messageClient = mockk(relaxed = true)
+            every { messageClient.sendMessage(any(), any(), any()) } returns Tasks.forResult(0)
+            every {
+                messageClient.sendMessage(any(), any(), any(), any())
+            } returns Tasks.forResult(0)
+            every { messageClient.addListener(any()) } answers {
+                messageListener = firstArg() as MessageClient.OnMessageReceivedListener
+                Tasks.forResult(null)
+            }
+
+            try {
+                base?.evaluate()
+            } finally { }
+        }
+    }
+}
