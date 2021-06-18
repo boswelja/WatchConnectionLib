01aff16fd4c923c3b33bc92a76ad1e31b4268c15
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-18 16:12:34 +1200

    Minor improvements to repeating function Also comment code

--- wearos/src/main/kotlin/com/boswelja/watchconnection/wearos/Helpers.kt
+++ wearos/src/main/kotlin/com/boswelja/watchconnection/wearos/Helpers.kt
@@ -2,12 +2,21 @@ package com.boswelja.watchconnection.wearos
 
 import kotlinx.coroutines.delay
 
-suspend fun repeating(
+/**
+ * Schedule a suspendable code block to be executed at a regular interval.
+ * @param interval The interval in milliseconds to execute the code block after. Must be greater
+ * than 0.
+ * @param initialDelay The initial delay in milliseconds before first execution. Values less than or
+ * equal to 0 are ignored.
+ * @param action The suspendable code block to be executed.
+ */
+internal suspend fun repeating(
     interval: Long,
     initialDelay: Long = 0,
     action: suspend () -> Unit
 ) {
-    delay(initialDelay)
+    if (interval <= 0) throw IllegalArgumentException("interval must be greater than 0")
+    if (initialDelay > 0) delay(initialDelay)
     while (true) {
         action()
         delay(interval)
