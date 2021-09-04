3ecf0861bb8a97bad7bdb91d60d8d741f9030a02
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-09-04 21:13:12 +1200

    Add messageClient extension

--- samples/wearos/src/main/kotlin/com/watchconnection/sample/Clients.kt
+++ samples/wearos/src/main/kotlin/com/watchconnection/sample/Clients.kt
@@ -1,11 +1,19 @@
 package com.watchconnection.sample
 
 import android.content.Context
+import com.boswelja.watchconnection.common.message.serialized.StringSerializer
 import com.boswelja.watchconnection.wearos.discovery.DiscoveryClient
+import com.boswelja.watchconnection.wearos.message.MessageClient
 
 fun Context.discoveryClient(): DiscoveryClient =
     DiscoveryClient(
         this,
         "watchconnection-sample",
         Capabilities.values().map { it.name }
+    )
+
+fun Context.messageClient(): MessageClient =
+    MessageClient(
+        this,
+        listOf(StringSerializer(setOf("message-path")))
     )
