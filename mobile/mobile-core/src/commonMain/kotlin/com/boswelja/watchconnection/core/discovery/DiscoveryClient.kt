8614812e48dcfa9a910978c32d476d639b5b57bb
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-09-19 12:59:40 +1200

    Add missing functions to DiscoveryClient.kt

--- mobile/mobile-core/src/commonMain/kotlin/com/boswelja/watchconnection/core/discovery/DiscoveryClient.kt
+++ mobile/mobile-core/src/commonMain/kotlin/com/boswelja/watchconnection/core/discovery/DiscoveryClient.kt
@@ -1,9 +1,9 @@
 package com.boswelja.watchconnection.core.discovery
 
 import com.boswelja.watchconnection.common.Watch
+import com.boswelja.watchconnection.common.discovery.ConnectionMode
 import com.boswelja.watchconnection.core.BaseClient
 import com.boswelja.watchconnection.core.Platform
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 
@@ -19,7 +19,6 @@ public class DiscoveryClient(
     /**
      * Get a [Flow] of all [Watch]es found by all [Platform]s.
      */
-    @ExperimentalCoroutinesApi
     public fun allWatches(): Flow<List<Watch>> =
         combine(platforms.values.map { it.allWatches() }) { flows ->
             val list = mutableListOf<Watch>()
@@ -36,5 +35,42 @@ public class DiscoveryClient(
      */
     public fun getCapabilitiesFor(watch: Watch): Flow<Set<String>>? {
         return platforms[watch.platform]?.getCapabilitiesFor(watch.internalId)
+    }
+
+    /**
+     * Check whether the given watch has a specified capability.
+     * @param watch The [Watch] to check against.
+     * @param capability The capability to look for.
+     * @return a [Flow] of [Boolean], where true indicates the watch has the capability.
+     */
+    public fun hasCapability(watch: Watch, capability: String): Flow<Boolean> {
+        val platform = platforms[watch.platform]
+        checkNotNull(platform) { "No platform found for watch $watch" }
+        return platform.hasCapability(watch, capability)
+    }
+
+    /**
+     * Get a [Flow] of [Watch]es with a given capability.
+     * @param capability The target capability.
+     * @return A [Flow] of [Watch]es that declare [capability].
+     */
+    public fun watchesWithCapability(capability: String): Flow<List<Watch>> =
+        combine(platforms.values.map { it.watchesWithCapability(capability) }) { flows ->
+            val list = mutableListOf<Watch>()
+            flows.forEach { watches ->
+                list += watches
+            }
+            list
+        }
+
+    /**
+     * Gets a [Flow] of [ConnectionMode] for the given watch. Use this to observe the connection
+     * mode of a watch.
+     * @param watch The [Watch] whose connection mode to observe.
+     */
+    public fun connectionModeFor(watch: Watch): Flow<ConnectionMode> {
+        val platform = platforms[watch.platform]
+        checkNotNull(platform) { "No platform found for watch $watch" }
+        return platform.connectionModeFor(watch)
     }
 }
