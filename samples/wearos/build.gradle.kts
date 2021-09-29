495734124aacd1fb1368e174b15b6aaa0a4605dd
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-09-29 21:30:14 +1300

    Don't minify debug

--- samples/wearos/build.gradle.kts
+++ samples/wearos/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     buildTypes {
         getByName("debug") {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
