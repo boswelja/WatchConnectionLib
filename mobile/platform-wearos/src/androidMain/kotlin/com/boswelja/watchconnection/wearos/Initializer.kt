702a7ef2376f23c6c134f641f97867b68c6a063e
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-10-02 17:53:42 +1300

    Create Initializers for platform-wearos

---
+++ mobile/platform-wearos/src/androidMain/kotlin/com/boswelja/watchconnection/wearos/Initializer.kt
@@ -0,0 +1,25 @@
+package com.boswelja.watchconnection.wearos
+
+import android.content.Context
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.api.AvailabilityException
+import com.google.android.gms.wearable.Wearable
+import kotlinx.coroutines.tasks.await
+
+/**
+ * Check whether Wear OS APIs are available.
+ * @return true if all Wear OS APIs are available, false otherwise
+ */
+public suspend fun Context.isWearOSAvailable(): Boolean {
+    val availabilityChecker = GoogleApiAvailability.getInstance()
+    return try {
+        availabilityChecker.checkApiAvailability(
+            Wearable.getCapabilityClient(this),
+            Wearable.getMessageClient(this),
+            Wearable.getNodeClient(this)
+        ).await()
+        true
+    } catch (e: AvailabilityException) {
+        false
+    }
+}
