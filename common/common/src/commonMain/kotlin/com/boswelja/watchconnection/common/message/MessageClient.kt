5e1794d8de786dadc2010105434c1638262e08b1
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-10-31 20:17:08 +1300

    Create MessageClient.kt common interface

---
+++ common/common/src/commonMain/kotlin/com/boswelja/watchconnection/common/message/MessageClient.kt
@@ -0,0 +1,25 @@
+package com.boswelja.watchconnection.common.message
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+
+public interface MessageClient {
+
+    /**
+     * A [Flow] of [ReceivedMessage]s received by all platforms. Messages collected here have no
+     * additional processing performed, and thus only contain the raw data in bytes.
+     */
+    @OptIn(ExperimentalCoroutinesApi::class)
+    public fun rawIncomingMessages(): Flow<ReceivedMessage<ByteArray?>>
+
+    /**
+     * Send a message to the device with the given UID.
+     * @param targetUid The target device UID to send the message to.
+     * @param message The [Message] to send.
+     * @return true if sending the message was successful, false otherwise.
+     */
+    public suspend fun sendMessage(
+        targetUid: String,
+        message: Message<ByteArray?>
+    ): Boolean
+}
--- mobile/mobile-core/src/commonMain/kotlin/com/boswelja/watchconnection/core/message/MessageClient.kt
+++ mobile/mobile-core/src/commonMain/kotlin/com/boswelja/watchconnection/core/message/MessageClient.kt
@@ -2,6 +2,7 @@ package com.boswelja.watchconnection.core.message
 
 import com.boswelja.watchconnection.common.Watch
 import com.boswelja.watchconnection.common.message.Message
+import com.boswelja.watchconnection.common.message.MessageClient
 import com.boswelja.watchconnection.common.message.MessageSerializer
 import com.boswelja.watchconnection.common.message.ReceivedMessage
 import com.boswelja.watchconnection.core.BaseClient
@@ -20,14 +21,15 @@ import kotlinx.coroutines.flow.merge
 public class MessageClient(
     private val serializers: List<MessageSerializer<*>> = listOf(),
     platforms: List<MessagePlatform>
-) : BaseClient<MessagePlatform>(platforms) {
+) : BaseClient<MessagePlatform>(platforms),
+    MessageClient {
 
     /**
      * A [Flow] of [ReceivedMessage]s received by all platforms. Messages collected here have no
      * additional processing performed, and thus only contain the raw data in bytes.
      */
     @OptIn(ExperimentalCoroutinesApi::class)
-    public fun rawIncomingMessages(): Flow<ReceivedMessage<ByteArray?>> = platforms.values
+    override fun rawIncomingMessages(): Flow<ReceivedMessage<ByteArray?>> = platforms.values
         .map { it.incomingMessages() }
         .merge()
 
@@ -90,7 +92,19 @@ public class MessageClient(
         val (platformId, internalId) = Watch.getInfoFromUid(targetUid)
         return sendMessage(platformId, internalId, message)
     }
-
+    /**
+     * Send a message to a [Watch]. See [MessagePlatform.sendMessage].
+     * @param targetUid The [Watch.uid] to send the message to.
+     * @param message The [Message] to send.
+     * @return true if sending the message was successful, false otherwise.
+     */
+    override suspend fun sendMessage(
+        targetUid: String,
+        message: Message<ByteArray?>
+    ): Boolean {
+        val (platformId, internalId) = Watch.getInfoFromUid(targetUid)
+        return sendMessage(platformId, internalId, message)
+    }
     /**
      * Send a message to a [Watch]. See [MessagePlatform.sendMessage].
      * @param target The [Watch] to send the message to.
